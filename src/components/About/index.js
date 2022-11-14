3d53e7d7851043a9145e65666acdf22127b88163
dusasrinivas <srinivasdsri1989@gmail.com>
2022-11-14 14:30:32 +0000

    commit

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+// Write your code here
+
+import Header from '../Header'
+
+import './index.css'
+
+const About = () => (
+  <div className="home-container">
+    <Header />
+    <img
+      alt="about"
+      className="home-image"
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+    />
+  </div>
+)
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+
+import Header from '../Header'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <img
+      alt="home"
+      className="home-image"
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+    />
+  </div>
+)
+export default Home
